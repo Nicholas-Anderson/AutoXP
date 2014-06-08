394ee0a18adbd1f16ca3752a2f7da73da267e9f4
sabrewolf <nicholas.anderson@live.com.au>
2014-06-09 09:54:32 +1000

    fixed stuff

--- src/me/sabrewolf/skyservers/pixelmon/Main.java
+++ src/me/sabrewolf/skyservers/pixelmon/Main.java
@@ -4,17 +4,16 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin 
-{
-	public void onEnable()
-	{
-		getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable()
-		{
-			public void run()
-			{
-				for(Player p : Bukkit.getOnlinePlayers()) {p.giveExp(1);}				
-			}
-		}, 0, 200);
+public class Main extends JavaPlugin {
+	public void onEnable() {
+		getServer().getScheduler().scheduleSyncRepeatingTask(this,
+				new Runnable() {
+					public void run() {
+						for (Player p : Bukkit.getOnlinePlayers()) {
+							p.giveExp(1);
+						}
+					}
+				}, 0, 200);
 	}
-	
+
 }
