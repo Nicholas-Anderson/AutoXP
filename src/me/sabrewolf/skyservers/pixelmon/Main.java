d396e017b131fa657a0c1de3018f6f51df9108d8
sabrewolf <nicholas.anderson@live.com.au>
2014-06-09 09:50:41 +1000

    first commit
    works?

---
+++ src/me/sabrewolf/skyservers/pixelmon/Main.java
@@ -0,0 +1,20 @@
+package me.sabrewolf.skyservers.pixelmon;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin 
+{
+	public void onEnable()
+	{
+		getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable()
+		{
+			public void run()
+			{
+				for(Player p : Bukkit.getOnlinePlayers()) {p.giveExp(1);}				
+			}
+		}, 0, 200);
+	}
+	
+}
