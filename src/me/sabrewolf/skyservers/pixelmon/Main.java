afbe87692ecc2438f7f890be44f6977a05f36812
sabrewolf <nicholas.anderson@live.com.au>
2014-06-09 10:29:55 +1000

    Changed Exp to Set

--- src/me/sabrewolf/skyservers/pixelmon/Main.java
+++ src/me/sabrewolf/skyservers/pixelmon/Main.java
@@ -4,28 +4,24 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin 
-{
-	public void onEnable() 
-	{
+public class Main extends JavaPlugin {
+	public void onEnable() {
 		getServer().getScheduler().scheduleSyncRepeatingTask(this,
 				new Runnable() {
 					public void run() {
 						giveXp();
-						}
+					}
 				}, 0, 100);
 
 	}
 
-	public void giveXp() 
-	{
-		for (Player p : Bukkit.getOnlinePlayers())
-			{
-				float CurrentExp = p.getExp();
-				if (CurrentExp > 150)
-				     p.giveExp(1);
-				else if (CurrentExp < 150)
-				     p.giveExp(3);
-			}
+	public void giveXp() {
+		for (Player p : Bukkit.getOnlinePlayers()) {
+			float CurrentExp = p.getExp();
+			if (CurrentExp > 150)
+				p.setExp(CurrentExp + 1);
+			else if (CurrentExp < 150)
+				p.setExp(CurrentExp + 3);
+		}
 	}
 }
