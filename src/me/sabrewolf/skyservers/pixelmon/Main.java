e43a94f11e2549ead01d69ba96257d53781e53c4
sabrewolf <nicholas.anderson@live.com.au>
2014-06-09 14:54:41 +1000

    Reworked code
    Depends essentials

--- src/me/sabrewolf/skyservers/pixelmon/Main.java
+++ src/me/sabrewolf/skyservers/pixelmon/Main.java
@@ -3,8 +3,13 @@ package me.sabrewolf.skyservers.pixelmon;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import com.earth2me.essentials.Essentials;
 
 public class Main extends JavaPlugin {
+	// public static ess = com.earth2me.essentials.commands.Commandexp;
+	Essentials ess = (Essentials) Bukkit.getServer().getPluginManager()
+			.getPlugin("Essentials");
+
 	public void onEnable() {
 		getServer().getScheduler().scheduleSyncRepeatingTask(this,
 				new Runnable() {
@@ -15,13 +20,17 @@ public class Main extends JavaPlugin {
 
 	}
 
+	// Initialise giving their Exp
 	public void giveXp() {
 		for (Player p : Bukkit.getOnlinePlayers()) {
-			float CurrentExp = p.getExp();
-			if (CurrentExp > 150)
-				p.setExp(CurrentExp + 1);
-			else if (CurrentExp < 150)
-				p.setExp(CurrentExp + 3);
+			float CurrentExp = p.getTotalExperience();
+			if (CurrentExp < 150) {
+
+				ess.getUser(p).giveExp(3);
+			} else {
+
+				ess.getUser(p).giveExp(1);
+			}
 		}
 	}
 }
