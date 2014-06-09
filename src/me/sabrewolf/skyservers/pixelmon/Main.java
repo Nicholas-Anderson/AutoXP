79427061de4419ffb81928dcea2b04f1f6ec680d
sabrewolf <nicholas.anderson@live.com.au>
2014-06-09 10:26:58 +1000

    Changed timings and amount of exp

--- src/me/sabrewolf/skyservers/pixelmon/Main.java
+++ src/me/sabrewolf/skyservers/pixelmon/Main.java
@@ -4,32 +4,28 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
-	public void onEnable() {
+public class Main extends JavaPlugin 
+{
+	public void onEnable() 
+	{
 		getServer().getScheduler().scheduleSyncRepeatingTask(this,
 				new Runnable() {
 					public void run() {
 						giveXp();
 						}
-
-					private void giveXp() {
-						// TODO Auto-generated method stub
-						for (Player p : Bukkit.getOnlinePlayers())
-						{
-							float CurrentExp = p.getExp();
-							if (CurrentExp > 300){
-								p.giveExp(1);
-							}
-							if (CurrentExp < 300){
-								p.giveExp(5);
-							}						
-						
-					}
-					}
-				}, 0, 200);
+				}, 0, 100);
 
 	}
 
-		
-	
+	public void giveXp() 
+	{
+		for (Player p : Bukkit.getOnlinePlayers())
+			{
+				float CurrentExp = p.getExp();
+				if (CurrentExp > 150)
+				     p.giveExp(1);
+				else if (CurrentExp < 150)
+				     p.giveExp(3);
+			}
+	}
 }
