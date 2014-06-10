5dfd95920ec47bc894185ef5d0ef7ec127cc24a8
pvmac2194 <pvmac1994@gmail.com>
2014-06-10 01:44:57 -0400

    Fixed the whole plugin.
    Added a set command (which also reloads), fixed reload, fixed the exp
    giving itself, fixed spacing, added a config and deleted the making of
    one in onEnable(), pretty much a rewrite.

--- src/me/sabrewolf/skyservers/pixelmon/Main.java
+++ src/me/sabrewolf/skyservers/pixelmon/Main.java
@@ -1,90 +1,137 @@
 package me.sabrewolf.skyservers.pixelmon;
 
-import java.io.File;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import com.earth2me.essentials.Essentials;
 
-public class Main extends JavaPlugin {
-	Essentials ess = (Essentials) Bukkit.getServer().getPluginManager()
-			.getPlugin("Essentials");
+public class Main extends JavaPlugin implements Listener
+	{
+		Essentials ess = (Essentials) Bukkit.getServer().getPluginManager()
+				.getPlugin("Essentials");
 
-	public void onEnable() {
-		File file = new File(getDataFolder() + File.separator + "config.yml");
-		if (!file.exists()) {
-			getLogger().info("Generating the config..");
-
-			getConfig().addDefault("time", 100);
-			getConfig().addDefault("minExpPoints", 150);
-			getConfig().addDefault("expGivenForUnderMin", 3);
-			getConfig().addDefault("expGivenForAboveMin", 1);
-			getConfig().options().copyDefaults(true);
-			saveConfig();
-		}
-		getServer().getScheduler().scheduleSyncRepeatingTask(this,
-				new Runnable() {
-					public void run() {
-						giveXp();
-					}
-				}, 0, getConfig().getInt("time")); // this is in ticks (time)
-
-	}
-	
-	// Initialise giving their Exp
-	public void giveXp() {
-		for (Player p : Bukkit.getOnlinePlayers()) {
-			float CurrentExp = p.getTotalExperience();
-			if (!(ess.getUser(p).isJailed() == true)) {
-				if ((!ess.getUser(p).isAfk() == true)) {
-					if (CurrentExp < getConfig().getInt("minExpPoints")) {
-						ess.getUser(p).giveExp(
-								getConfig().getInt("expGivenForUnderMin"));
-					} else {
-						ess.getUser(p).giveExp(
-								getConfig().getInt("expGivenForAboveMin"));
-					}
-				}
-			}
-		}
-	}
-
-	// Commands
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label,
-			String[] args) {
-		// Information on commands
-		if (cmd.getName().equalsIgnoreCase("autoexp")) {
-			if (args.length == 0) {
-				sender.sendMessage("§b=-----------------+  §9 AutoExp §b  +-----------------=");
-				sender.sendMessage("§b= §3Available commands§f:");
-				if (sender.hasPermission("autoexp.reload")) {
-					sender.sendMessage("§b=  §5  - §a /autoexp §creload  §f-§e  Reloads the configration.");
-				}
-				sender.sendMessage("§b=---------------------------------------------------=");
-				return true;
-			}
-		}
-		// Reload command
-		if (args[0].equalsIgnoreCase("reload")) {
-
-			if (sender.hasPermission("autoexp.reload")) {
-				reloadConfig();
+		@Override
+		public void onEnable()
+			{
+				Bukkit.getPluginManager().registerEvents(this, this);
+				getConfig().options().copyDefaults(true);
 				saveConfig();
-				sender.sendMessage(ChatColor.GREEN
-						+ "Configuration has been Reloaded!");
-				return true;
+
+				initializeExpGiving();
 			}
-			sender.sendMessage(ChatColor.RED
-					+ "You're not allowed to use that!");
-			return true;
-		}
-		return false;
+
+		public void initializeExpGiving()
+			{
+				getServer().getScheduler().scheduleSyncRepeatingTask(this,
+						new Runnable()
+							{
+								public void run()
+									{
+										giveXp();
+									}
+							}, 0, (getConfig().getInt("timeInSeconds") * 20)); // this is in
+																																	// ticks
+				// (time)
+			}
+
+		// Initialise giving their Exp
+		public void giveXp()
+			{
+				for (Player p : Bukkit.getOnlinePlayers())
+					{
+						float CurrentExp = p.getTotalExperience();
+						if (ess.getUser(p).isJailed() != true)
+							{
+								if (ess.getUser(p).isAfk() != true)
+									{
+										if (CurrentExp < getConfig().getInt("minExpPoints"))
+											{
+												p.giveExp(getConfig().getInt("expGivenForUnderMin"));
+											}
+										else
+											{
+												p.giveExp(getConfig().getInt("expGivenForAboveMin"));
+											}
+									}
+							}
+					}
+			}
+
+		// Commands
+		@Override
+		public boolean onCommand(CommandSender sender, Command cmd, String label,
+				String[] args)
+			{
+				// Information on commands
+				if (cmd.getName().equalsIgnoreCase("autoexp"))
+					{
+						if (args.length == 0)
+							{
+								sender
+										.sendMessage("§b=-----------------+  §9 AutoExp §b  +-----------------=");
+								sender.sendMessage("§b= §3Available commands§f:");
+								if (sender.hasPermission("autoexp.reload"))
+									sender
+											.sendMessage("§b=  §5  - §a /autoexp §creload  §f-§e  Reloads the configration.");
+								if (sender.hasPermission("autoexp.setval"))
+									sender
+											.sendMessage("§b=  §5  - §a /autoexp §csetval (val) (amount) §f-§e  Sets a value in the config.");
+								if (sender.hasPermission("autoexp.setval"))
+									sender
+											.sendMessage("§b=---------------------------------------------------=");
+								return true;
+							}
+					}
+				// Reload command
+				if (args[0].equalsIgnoreCase("reload"))
+					{
+						if (sender.hasPermission("autoexp.reload"))
+							{
+
+								this.reloadConfig();
+								sender.sendMessage(ChatColor.GREEN
+										+ "Configuration has been Reloaded!");
+								return true;
+							}
+						sender.sendMessage(ChatColor.RED
+								+ "You're not allowed to use that!");
+						Bukkit.getPluginManager().disablePlugin(this);
+						Bukkit.getPluginManager().enablePlugin(this);
+						return true;
+					}
+				else
+					if (args[0].equalsIgnoreCase("setval"))
+						{
+							if (sender.hasPermission("autoexp.setval"))
+								{
+									if (args.length == 1 || args.length == 2)
+										{
+											sender
+													.sendMessage(ChatColor.RED
+															+ "Usage: /autoexp setval (expGivenForAboveMin / expGivenForUnderMin / minExpPoints / timeInSeconds) (value)");
+										}
+									else
+										if (args.length > 1)
+											{
+												int num = Integer.parseInt(args[2]);
+												this.getConfig().set(args[1], num);
+												this.saveConfig();
+												this.reloadConfig();
+												sender.sendMessage(ChatColor.GREEN
+														+ "Configuration value \"" + args[1]
+														+ "\" set to \"" + args[2] + "\"");
+												Bukkit.getPluginManager().disablePlugin(this);
+												Bukkit.getPluginManager().enablePlugin(this);
+											}
+								}
+							return true;
+						}
+				return false;
+
+			}
 
 	}
-
-}
